a0c9cc6fbfc518d2d5f9bc08e28c43a539aae155
SahaidakVladyslav <Svg.231@gmail.com>
2022-02-21 20:14:48 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './sass/main.scss';
 
-import "./js/slider";
 import "./js/modal";
 import "./js/mobile-menu";
-
 // import 'animate.css';
+
+
 
--- src/js/slider.js
+++
@@ -1,8 +0,0 @@
-$('.slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        fade: true,
-        cssEase: 'linear',
-        slidesToScroll: 1,
-      });
