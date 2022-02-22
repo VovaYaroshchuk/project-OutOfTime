2596df6db914752f40815d4ee38383384448efbf
Hintln <andrejlityprog@gmail.com>
2022-02-22 18:58:56 +0200

    name(modal)

--- src/js/about-modal.js
+++ src/js/about-modal.js
@@ -7,7 +7,7 @@
   const refs = {
     openModalBtn: document.querySelector('[about-modal-open]'),
     closeModalBtn: document.querySelector('[about-modal-close]'),
-    modal: document.querySelector('[about-data-modal]'),
+    modal: document.querySelector('[about-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
