c98233adca516b796a51697500ad4183da2a76e6
Vova Yaroshchuk <voyaroshchuk@gmail.com>
2022-02-22 21:49:34 +0200

    fix

--- src/js/poduct-modal.js
+++ src/js/poduct-modal.js
@@ -1,16 +1,44 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open1]'),
+    closeModalBtn: document.querySelector('[data-modal-close1]'),
+    modal: document.querySelector('[data-modal1]'),
+  };
 
-    (() => {
-      const refs = {
-        openModalBtn: document.querySelector('[data-modal-open1]'),
-        closeModalBtn: document.querySelector('[data-modal-close1]'),
-        modal: document.querySelector('[data-modal1]'),
-      };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-      refs.openModalBtn.addEventListener('click', toggleModal);
-      refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--is-hidden');
+  }
+})();
 
-      function toggleModal() {
-        refs.modal.classList.toggle('backdrop--is-hidden');
-      }
-    })();
- 
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open2]'),
+    closeModalBtn: document.querySelector('[data-modal-close2]'),
+    modal: document.querySelector('[data-modal2]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open3]'),
+    closeModalBtn: document.querySelector('[data-modal-close3]'),
+    modal: document.querySelector('[data-modal3]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--is-hidden');
+  }
+})();
