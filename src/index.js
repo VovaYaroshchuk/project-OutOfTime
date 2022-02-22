b24bdb49a0125a13af89cbac34a693c3f3bf5a9a
Vova Yaroshchuk <voyaroshchuk@gmail.com>
2022-02-22 13:39:41 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import './sass/main.scss';
 import './animate.css';
 
+import "./js/header-modal";
+import "./js/menu";
+
 import "./js/modal";
 import "./js/slider";
-import "./js/modal";
+// import "./js/modal";
 import "./js/mobile-menu";
 import "./js/poduct-modal";
 import "./js/wow.min";
 import "./js/contacts";
-import "./js/header-modal";
-import "./js/menu";
+
 
 
 
